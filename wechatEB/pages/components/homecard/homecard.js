1e26952f23f8bd06faa3fa0dfc9e284f16b4596d
GDYYYY <48650628+GDYYYY@users.noreply.github.com>
2020-05-15 18:41:05 +0800

    Create homecard.js

---
+++ wechatEB/pages/components/homecard/homecard.js
@@ -0,0 +1,39 @@
+Component({
+  properties: {
+    b_id:{
+      type: String,
+      value: "",
+    }
+  },
+  data: {
+    url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+    title: "title",
+    author: "author",
+    price: "price",
+    u_id: "",
+    // b_id: "",
+    num: "",
+  },
+  ready:function () {
+    var that = this;
+    console.log(that.properties.b_id);
+    wx.request({
+      url: 'http://localhost:8080/getBook?b_id='+ that.properties.b_id,
+      method: "GET",
+      header: {
+        'content-type': 'application/json',
+      },
+      success(resp) {
+        console.log(resp);
+        that.setData({
+          author: resp.data.author+'\n',
+          title: resp.data.name+'\n',
+          url: resp.data.image,
+          price: resp.data.price,
+        });
+        console.log("sss");
+      }
+    })
+  },
+
+})
