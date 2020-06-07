fc83b306d68e34d7c2078e23933b4a8c5f287aea
GDYYYY <48650628+GDYYYY@users.noreply.github.com>
2020-06-07 14:33:49 +0800

    Add files via upload

---
+++ rnEB/components/HomeCard.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet, Dimensions, Image,TouchableOpacity} from 'react-native';
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+    base: {
+        width: width/5*2,
+        // height: height/4,
+        margin:width/40,
+        marginBottom:0,
+        padding:width/80,
+        alignItems:'center',
+        borderRadius:5,
+    },
+    background: {
+        backgroundColor: '#fff',
+    },
+    price: {
+        color: '#1989fa',
+    },
+    author: {
+        color: 'rgb(96, 125, 163)',
+    },
+});
+export default class HomeCard extends Component {
+
+    render() {
+        return (
+            <View style={[styles.base,styles.background]}  >
+                <Image source={{uri: this.props.book.image}} style={{width: width/10*3,height:height/5}} ></Image>
+                <Text>{this.props.book.name}</Text>
+                <Text style={[styles.author]}>{this.props.book.author}</Text>
+                <Text style={[styles.price]}>￥{this.props.book.price}</Text>
+            </View>
+        );
+    }
+}
