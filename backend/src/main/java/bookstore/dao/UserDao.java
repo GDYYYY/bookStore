637e986fa45eba26805aac8c2b0effbe44eb61c8
GDYYYY <48650628+GDYYYY@users.noreply.github.com>
2020-07-04 20:54:41 +0800

    Add files via upload

--- backend/src/main/java/bookstore/dao/UserDao.java
+++ backend/src/main/java/bookstore/dao/UserDao.java
@@ -7,6 +7,7 @@ public interface UserDao {
     User ifUser(String username, String password);
     User existUser(String username);
     User setUser(User user);
+    User saveUser(User user);
     User getUser(Integer u_id);
     List<User> getUsers();
     User addIcon(User user);
