2c0f5e7c7a4b66adbaf8274eed862029a15751c5
GDYYYY <48650628+GDYYYY@users.noreply.github.com>
2020-07-04 21:13:26 +0800

    Add files via upload

--- wechatEB/pages/components/bookcard/bookcard.js
+++ wechatEB/pages/components/bookcard/bookcard.js
@@ -1,3 +1,4 @@
+const app=getApp();
 Component({
   properties: {
     b_id:{
@@ -5,6 +6,10 @@ Component({
       value: "",
     },
     num:{
+      type: Number,
+      value: "",
+    },
+    role:{
       type: Number,
       value: "",
     },
@@ -30,12 +35,40 @@ Component({
         that.setData({
           author: resp.data.author+'\n',
           title: resp.data.name+'\n',
-          url: resp.data.image,
+          url: resp.data.bookInfo.image,
           price: resp.data.price,
         });
         console.log("sss");
       }
     })
   },
-
+  methods:{
+  toDetails: function () {
+    console.log(this.properties.b_id);
+    app.globalData.b_id=this.properties.b_id;
+    wx.navigateTo({
+      url: '../details/details',
+    })
+  },
+  noop(){},
+  onChange:function(event){
+    const that=this;
+    console.log(event.detail);
+    console.log(that.properties.num);
+    this.triggerEvent('reCheck',that.properties.num)
+    wx.request({
+      url: 'http://localhost:8080/updateCart?u_id='+ app.globalData.user.u_id+'&b_id='+that.properties.b_id+'&num='+event.detail,
+      method: "POST",
+      header: {
+        'content-type': 'application/json',
+      },
+      success(resp) {
+        console.log(resp);
+        // that.setData({
+        //   book:resp.data
+        // });
+      }
+    })
+  }
+}
 })
